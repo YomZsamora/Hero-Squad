48156720fd336b8228f32a07582d8cd9587edda8
yomZsamora <samora.y@adzumi.co.ke>
2018-05-13 16:02:20 +0300

    Create squad form done.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,7 @@ public class App {
     	get("/", (request, response) -> {
       		Map<String, Object> model = new HashMap<String, Object>();
       		model.put("hero_squad", request.session().attribute("hero_squad"));
+      		model.put("squad_hero", request.session().attribute("squad_hero"));
       		model.put("template", "templates/index.vtl");
       		return new ModelAndView(model, layout);
     	}, new VelocityTemplateEngine());
@@ -37,7 +38,24 @@ public class App {
     		model.put("template", "templates/hero_added.vtl");
     		return new ModelAndView(model, layout);
    		}, new VelocityTemplateEngine());
+
+   		post("/createSquad", (request, response) -> {
+    		Map<String, Object> model = new HashMap<String, Object>();
+
+		    ArrayList<Squad> squads = request.session().attribute("squad_hero");
+		    if (squads == null) {
+		      	squads = new ArrayList<Squad>();
+		      	request.session().attribute("squad_hero", squads);
+		    }
+
+    		String squadName = request.queryParams("squad_name");
+    		String squadSize = request.queryParams("squad_size");
+    		String squadCause = request.queryParams("squad_cause");
+    		Squad newSquad = new Squad(squadName,squadSize,squadCause);
+    		squads.add(newSquad);
+
+    		model.put("template", "templates/squad_added.vtl");
+    		return new ModelAndView(model, layout);
+   		}, new VelocityTemplateEngine());
   	}
-
-
 }
--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Squad {
    private List<Hero> squadMembers = new ArrayList<Hero>();
    private String squadName;
-   private int maxSize;
+   private String maxSize;
    private String squadCause;
 
-   public Squad (String squad_name, int squad_size, String squad_cause){
+   public Squad (String squad_name, String squad_size, String squad_cause){
       squadName = squad_name;
       maxSize = squad_size;
       squad_cause = squad_cause;
@@ -16,11 +16,11 @@ public class Squad {
       return squadMembers;
    }
 
-   public int getSquadSize(){
-      return squadMembers.size();
-   }
+   // public String getSquadSize(){
+   //    return squadMembers.size();
+   // }
 
-   public int getMaxSize(){
+   public String getMaxSize(){
       return maxSize;
    }
 
@@ -32,12 +32,12 @@ public class Squad {
       return squadCause;
    }
 
-   public String addHero(Hero newHero){
-      if (squadMembers.size() < maxSize){
-         squadMembers.add(newHero);
-         return "Successfully added hero";
-      } else {
-         return "Squad is at maximum size";
-      }
-   }
+   // public String addHero(Hero newHero){
+   //    if (squadMembers.size() < maxSize){
+   //       squadMembers.add(newHero);
+   //       return "Successfully added hero";
+   //    } else {
+   //       return "Squad is at maximum size";
+   //    }
+   // }
 }
